72bcb63e7ef4b14d0fce77731841de3406224084
MdZahid-wb <zahidmd9830@gmail.com>
2022-11-09 17:07:30 +0530

    15 commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -185,7 +185,9 @@ routes.post("/callback", (req, res) => {
 __dirname = path.resolve();
 if (process.env.NODE_ENV == "production") {
   console.log("production-mode.......");
-  app.use(express.static(path.join(__dirname, "frontend/build/")));
+  app.use(
+    express.static(path.join(__dirname, "frontend/build/static/index.html"))
+  );
   app.get("*", (req, res) => {
     res.sendFile(
       path.resolve(__dirname, "frontend", "build", "static", "index.html")
